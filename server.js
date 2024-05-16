7448a8fed45fd42c0a647af40c848625f0923e9f
F4B14NM4RT1N3Z <daborfabianmar@gmail.com>
2024-05-15 21:49:22 -0400

    changes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const app = express()
 
 
 
-app.get('/',(req,res) => {
-    res.send("Renato gay putito");
+app.get('/principal',(req,res) => {
+    res.sendFile('./principal.html',
+    {root: __dirname})
 
 })
 
